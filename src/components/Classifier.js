f705fb04a5609ed5aeb9df84e10764bdfb0e2b77
Allen Goodman <allen.goodman@icloud.com>
2018-04-22 20:07:33 +0200

    classifier component

---
+++ src/components/Classifier.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Classifier extends Component {
+  render() {
+    return <br />;
+  }
+}
+
+export default Classifier;
---
+++ src/components/Classifier.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Classifier from './Classifier';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Classifier />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
