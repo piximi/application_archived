f99d6c1118b1f899895d82bc010d9689d2fb3229
Allen Goodman <allen.goodman@icloud.com>
2018-05-02 13:43:21 -0400

    cleanup

--- src/components/Primary.js
+++ src/components/Primary.js
@@ -1,31 +1,29 @@
 import { AppBar, Button, Toolbar, Typography } from 'material-ui';
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Primary.css';
 import { withStyles } from 'material-ui/styles/index';
 import Download from '@axetroy/react-download';
 
-class Primary extends Component {
-  render() {
-    const { classes } = this.props;
+const Primary = props => {
+  const { classes } = props;
 
-    return (
-      <AppBar position="fixed" className={classes.appBar} color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit" noWrap>
-            Cyto
-          </Typography>
+  return (
+    <AppBar position="fixed" className={classes.appBar} color="default">
+      <Toolbar>
+        <Typography variant="title" color="inherit" noWrap>
+          Cyto
+        </Typography>
 
-          <Button onClick={this.train}>Run</Button>
+        <Button onClick={this.train}>Run</Button>
 
-          <input onChange={this.open} type="file" />
+        <input onChange={this.open} type="file" />
 
-          <Download file="example.cyto" content={JSON.stringify(this.state)}>
-            <Button onClick={this.save}>Save</Button>
-          </Download>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+        <Download file="example.cyto" content={JSON.stringify(this.state)}>
+          <Button onClick={this.save}>Save</Button>
+        </Download>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default withStyles(styles)(Primary);
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,36 @@
 import { Divider, Drawer, Grid, Toolbar } from 'material-ui';
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Primary.css';
 import { withStyles } from 'material-ui/styles/index';
 import Categories from './Categories';
 
-class Sidebar extends Component {
-  render() {
-    const {
-      categories,
-      categoryOnChange,
-      categoryOnNameChange,
-      createCategory,
-      classes
-    } = this.props;
+const Sidebar = props => {
+  const {
+    categories,
+    categoryOnChange,
+    categoryOnNameChange,
+    createCategory,
+    classes
+  } = props;
 
-    return (
-      <Grid item xs={3}>
-        <Drawer classes={{ paper: classes.drawerPaper }} variant="permanent">
-          <div className={classes.toolbar} />
+  return (
+    <Grid item xs={3}>
+      <Drawer classes={{ paper: classes.drawerPaper }} variant="permanent">
+        <div className={classes.toolbar} />
 
-          <Toolbar />
+        <Toolbar />
 
-          <Divider />
+        <Divider />
 
-          <Categories
-            categories={categories}
-            categoryOnChange={categoryOnChange}
-            categoryOnNameChange={categoryOnNameChange}
-            onClick={createCategory}
-          />
-        </Drawer>
-      </Grid>
-    );
-  }
-}
+        <Categories
+          categories={categories}
+          categoryOnChange={categoryOnChange}
+          categoryOnNameChange={categoryOnNameChange}
+          onClick={createCategory}
+        />
+      </Drawer>
+    </Grid>
+  );
+};
 
 export default withStyles(styles)(Sidebar);
