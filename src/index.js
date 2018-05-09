43fa553a4df196457c9df96f64750a6213a194db
Allen Goodman <allen.goodman@icloud.com>
2018-05-09 14:01:09 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import data from './images/mnist';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const demo = {
+  categories: data.categories,
+  images: [],
+  settings: {}
+};
+
+const store = createStore(reducer, demo);
 
 ReactDOM.render(
   <Provider store={store}>
