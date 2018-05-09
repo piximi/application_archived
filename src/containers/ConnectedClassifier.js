1c8ce51cfed81c6d00b62a2210102ab323facf56
Allen Goodman <allen.goodman@icloud.com>
2018-05-09 16:08:28 -0400

    cleanup

--- src/containers/ConnectedClassifier.js
+++ src/containers/ConnectedClassifier.js
@@ -1,11 +1,38 @@
 import { connect } from 'react-redux';
 
+import {
+  fitClassifierAction,
+  openClassifierAction,
+  saveClassifierAction
+} from '../actions/classifier';
 import Classifier from '../components/Classifier';
 
 const mapStateToProps = state => {
   return state;
 };
 
-const ConnectedClassifier = connect(mapStateToProps)(Classifier);
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    fit: () => {
+      const pathname = '';
+
+      dispatch(fitClassifierAction(pathname));
+    },
+    open: event => {
+      const pathname = '';
+
+      dispatch(openClassifierAction(pathname));
+    },
+    save: event => {
+      const pathname = '';
+
+      dispatch(saveClassifierAction(pathname));
+    }
+  };
+};
+
+const ConnectedClassifier = connect(mapStateToProps, mapDispatchToProps())(
+  Classifier
+);
 
 export default ConnectedClassifier;
