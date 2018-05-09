b7130438f208a472b25d95e81da82b4e3f89cd5c
Allen Goodman <allen.goodman@icloud.com>
2018-05-09 14:47:45 -0400

    cleanup

--- src/containers/ConnectedClassifier.js
+++ src/containers/ConnectedClassifier.js
@@ -1,14 +1,8 @@
 import { connect } from 'react-redux';
 
 import Classifier from '../components/Classifier';
-import data from '../images/mnist.json';
 
 const mapStateToProps = state => {
-  // categories={data.categories}
-  // images={data.images}
-  // settings={data.settings}
-  state = data;
-
   return state;
 };
 
