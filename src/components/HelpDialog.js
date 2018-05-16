914836373ef8fd08b4741e79b2c1d661057f141f
Allen Goodman <allen.goodman@icloud.com>
2018-05-16 11:49:12 -0700

    HelpDialog

---
+++ src/components/HelpDialog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './HelpDialog.css';
+import { withStyles } from 'material-ui/styles/index';
+
+const HelpDialog = props => {
+  return <div />;
+};
+
+export default withStyles(styles)(HelpDialog);
