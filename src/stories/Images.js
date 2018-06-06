ed493f64cb7a750e315849230c85ccc32a88ec37
Allen Goodman <allen.goodman@icloud.com>
2018-06-06 16:07:13 -0400

    stories

--- src/stories/Images.js
+++ src/stories/Images.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Images from '../components/Images';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import data from '../images/mnist';
+import reducer from '../reducers';
+import dataImages from '../images/stock';
 
-storiesOf('Images', module).add('example', () => <Images />);
+const fixture = {
+  categories: data.categories,
+  images: {
+    images: dataImages.images,
+    imageByteStrings: dataImages.imageByteStrings
+  },
+  settings: data.settings
+};
+
+const store = createStore(reducer, fixture);
+
+storiesOf('Images', module).add('example', () => (
+  <Provider store={store}>
+    <Images />
+  </Provider>
+));
--- src/stories/SettingsDialog.js
+++ src/stories/SettingsDialog.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import SettingsDialog from '../components/SettingsDialog';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import data from '../images/mnist';
+import reducer from '../reducers';
+import dataImages from '../images/stock';
+
+const fixture = {
+  categories: data.categories,
+  images: {
+    images: dataImages.images,
+    imageByteStrings: dataImages.imageByteStrings
+  },
+  settings: data.settings
+};
+
+const store = createStore(reducer, fixture);
 
 storiesOf('SettingsDialog', module).add('example', () => (
-  <SettingsDialog open={true} />
+  <Provider store={store}>
+    <SettingsDialog open={true} settings={fixture.settings} />
+  </Provider>
 ));
