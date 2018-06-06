e761c460dab0d37e84ef4bc7038b332f6fec02ec
Allen Goodman <allen.goodman@icloud.com>
2018-06-06 15:59:52 -0400

    stories

--- src/stories/Primary.js
+++ src/stories/Primary.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Primary from '../components/Primary';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import data from '../images/mnist';
+import reducer from '../reducers';
+import dataImages from '../images/stock';
 
-storiesOf('Primary', module).add('example', () => <Primary />);
+const demo = {
+  categories: data.categories,
+  images: {
+    images: dataImages.images,
+    imageByteStrings: dataImages.imageByteStrings
+  },
+  settings: data.settings
+};
+
+const store = createStore(reducer, demo);
+
+storiesOf('Primary', module).add('example', () => (
+  <Provider store={store}>
+    <Primary />
+  </Provider>
+));
