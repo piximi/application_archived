c2608b42215f16a0e7cffbdcef7a5a81a4b2e2e8
Allen Goodman <allen.goodman@icloud.com>
2018-06-12 12:16:03 -0400

    settings page

--- src/components/SettingsDialog/SettingsDialog.js
+++ src/components/SettingsDialog/SettingsDialog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styles from './SettingsDialog.css';
 import { withStyles } from 'material-ui/styles/index';
-import { AppBar, Dialog, Tab, Tabs } from 'material-ui';
+import { AppBar, Dialog, Slide, Tab, Tabs } from 'material-ui';
 import SettingsDialogTabContainer from '../SettingsDialogTabContainer/SettingsDialogTabContainer';
 import Settings from '../Settings/Settings';
 
@@ -18,26 +18,13 @@ class SettingsDialog extends Component {
     const { classes, onClose, open } = this.props;
 
     return (
-      <Dialog fullScreen open={open} onClose={onClose}>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={onClose}
+        TransitionComponent={<Slide direction="up" {...this.props} />}
+      >
         <Settings onClose={onClose} />
-        {/*<div className={classes.root}>*/}
-        {/*<AppBar position="static">*/}
-        {/*<Tabs value={this.state.tab} onChange={this.onChange} fullWidth>*/}
-        {/*<Tab label="A" />*/}
-        {/*<Tab label="B" />*/}
-        {/*<Tab label="C" />*/}
-        {/*</Tabs>*/}
-        {/*</AppBar>*/}
-        {/*{this.state.tab === 0 && (*/}
-        {/*<SettingsDialogTabContainer>A</SettingsDialogTabContainer>*/}
-        {/*)}*/}
-        {/*{this.state.tab === 1 && (*/}
-        {/*<SettingsDialogTabContainer>B</SettingsDialogTabContainer>*/}
-        {/*)}*/}
-        {/*{this.state.tab === 2 && (*/}
-        {/*<SettingsDialogTabContainer>C</SettingsDialogTabContainer>*/}
-        {/*)}*/}
-        {/*</div>*/}
       </Dialog>
     );
   }
