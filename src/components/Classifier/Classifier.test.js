0c7d98f0c75de3d7a2653005f09e1c1b2f8501b0
Allen Goodman <allen.goodman@icloud.com>
2018-09-26 14:04:28 -0400

    test cleanup

--- src/components/Classifier/Classifier.test.js
+++ src/components/Classifier/Classifier.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Classifier from './Classifier';
-
-it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<Classifier />, div);
-  // ReactDOM.unmountComponentAtNode(div);
+test('addition', () => {
+  expect(1 + 1).toBe(2);
 });
