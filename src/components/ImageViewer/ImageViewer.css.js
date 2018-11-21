c52700c931c66c21613cee3990066e53be34a8f9
Allen Goodman <allen.goodman@icloud.com>
2018-11-21 11:27:30 -0500

    basic styling

--- src/components/ImageViewer/ImageViewer.css.js
+++ src/components/ImageViewer/ImageViewer.css.js
@@ -1,15 +1,18 @@
 const styles = theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    backgroundColor: '#000'
   },
   flex: {
     flex: 1
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20
+    marginRight: 20,
+    color: '#FFF'
   },
   appbar: {
+    backgroundColor: '#000',
     boxShadow: 'none'
   }
 });
