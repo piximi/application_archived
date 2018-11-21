182b772a925d50e589077d610c9ef593f4c22a25
Allen Goodman <allen.goodman@icloud.com>
2018-11-21 14:05:40 -0500

    drawers

--- src/components/ImageViewer/ImageViewerChannelDrawer/ImageViewerCannelDrawer.js
+++ src/components/ImageViewer/ImageViewerChannelDrawer/ImageViewerCannelDrawer.js
@@ -1,3 +1,28 @@
-const styles = theme => ({});
+import React, { Component } from 'react';
+import styles from './ImageViewer.css';
+import { withStyles } from '@material-ui/core/styles';
 
-export default styles;
+class ImageViewerChannelDrawer extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Drawer
+        anchor="right"
+        open={this.state.right}
+        onClose={this.toggleDrawer('right', false)}
+      >
+        <div
+          tabIndex={0}
+          role="button"
+          onClick={this.toggleDrawer('right', false)}
+          onKeyDown={this.toggleDrawer('right', false)}
+        />
+      </Drawer>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(
+  ImageViewerChannelDrawer
+);
---
+++ src/components/ImageViewer/ImageViewerExposureDrawer/ImageViewerExposureDrawer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import styles from './ImageViewer.css';
+import { withStyles } from '@material-ui/core/styles';
+
+class ImageViewerExposureDrawer extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Drawer
+        anchor="right"
+        open={this.state.right}
+        onClose={this.toggleDrawer('right', false)}
+      >
+        <div
+          tabIndex={0}
+          role="button"
+          onClick={this.toggleDrawer('right', false)}
+          onKeyDown={this.toggleDrawer('right', false)}
+        />
+      </Drawer>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(
+  ImageViewerExposureDrawer
+);
