94856c2e0225272b1cfd7b6d5f3731651ceedbca
Christoph Friedrich <Christoph.Friedrich@trackplus.com>
2018-11-30 15:18:21 -0500

    Fix Aspect ratio

--- src/components/Gallery/Image.js
+++ src/components/Gallery/Image.js
@@ -1,49 +1,82 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
-class Image extends PureComponent {
+class Image extends Component {
   constructor() {
     super();
-    this.state = { imageStatus: 'loading' };
-    this.myRef = React.createRef();
+    this.state = {
+      imageStatus: 'loading',
+      image: null,
+      imgHeight: null,
+      imgWidth: null
+    };
+    this.canvas = React.createRef();
   }
 
-  handleImageLoaded(e) {
-    const canvas = this.myRef.current;
-    const image = e.target;
-    const context = canvas.getContext('2d');
-    canvas.height = this.props.height * 0.9;
-    canvas.width = this.props.width * 0.9;
-    const ratio = Math.min(
-      canvas.width / image.width,
-      canvas.height / image.height
-    );
-    canvas.height = image.height * ratio;
-    canvas.width = image.width * ratio;
-    context.drawImage(image, 0, 0, image.width * ratio, image.height * ratio);
-    image.style.height = '0px';
-    this.setState({ imageStatus: 'loaded' });
+  // Make sure component is updated
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.width === nextProps.width) {
+      return true;
+    }
   }
+
+  // Set image data as state
+  handleLoadedImage = e => {
+    const image = e.target;
+    const width = image.width;
+    const height = image.height;
+    this.setState({
+      imageStatus: 'loaded',
+      image: image,
+      imgHeight: height,
+      imgWidth: width
+    });
+    image.style.height = '0px';
+    this.draw();
+  };
 
   handleImageErrored() {
     this.setState({ imageStatus: 'failed to load' });
   }
 
+  // Draw canvas
+  draw() {
+    if (this.state.imageStatus === 'loaded') {
+      const canvas = this.canvas.current;
+      const context = canvas.getContext('2d');
+      canvas.height = this.props.height * 0.9;
+      canvas.width = this.props.width * 0.9;
+      const ratio = Math.min(
+        canvas.width / this.state.imgWidth,
+        canvas.height / this.state.imgHeight
+      );
+      canvas.height = this.state.imgHeight * ratio;
+      canvas.width = this.state.imgWidth * ratio;
+      context.drawImage(
+        this.state.image,
+        0,
+        0,
+        this.state.imgWidth * ratio,
+        this.state.imgHeight * ratio
+      );
+    }
+  }
+
   render() {
     const { src, openImageViewerDialog } = this.props;
+    this.draw();
     return (
       <React.Fragment>
         <canvas
           onDoubleClick={openImageViewerDialog}
           type={'selectableElement'}
           style={{ verticalAlign: 'middle', padding: '2px' }}
-          ref={this.myRef}
+          ref={this.canvas}
           height={this.props.height}
           width={this.props.width}
         />
         <img
-          onLoad={this.handleImageLoaded.bind(this)}
+          onLoad={this.handleLoadedImage}
           onError={this.handleImageErrored.bind(this)}
-          ref="image"
           alt="foo"
           src={src}
           style={{ visibility: 'hidden' }}
