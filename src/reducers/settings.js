42ce2c20b8b33870775751652b432ad27fa6b673
Allen Goodman <allen.goodman@icloud.com>
2018-12-11 13:11:58 -0500

    removed upload dialog state from redux

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,4 +1,4 @@
-import { TOGGLE_UPLOAD_DIALOG, TOOGLE_NEW_IMAGES_EVENT } from '../constants';
+import { TOOGLE_NEW_IMAGES_EVENT } from '../constants';
 
 const settings = (state = {}, action) => {
   switch (action.type) {
@@ -6,13 +6,6 @@ const settings = (state = {}, action) => {
       return {
         ...state,
         uploadedNewImagesEvent: !state.uploadedNewImagesEvent
-      };
-    case TOGGLE_UPLOAD_DIALOG:
-      return {
-        ...state,
-        upload: {
-          toggled: !state.upload.toggled
-        }
       };
     default:
       return state;
