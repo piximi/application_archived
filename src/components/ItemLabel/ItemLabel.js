69ac54e7098a1d9f53d17e660d96a34b0a5c6aad
Allen Goodman <allen.goodman@icloud.com>
2018-12-12 16:00:34 -0500

    ItemCategoryMenu

--- src/components/ItemLabel/ItemLabel.js
+++ src/components/ItemLabel/ItemLabel.js
@@ -3,19 +3,47 @@ import LabelIcon from '@material-ui/icons/Label';
 import styles from './ItemLabel.css';
 import { withStyles } from '@material-ui/core/styles';
 import { IconButton } from '@material-ui/core';
+import ItemCategoryMenu from '../ItemCategoryMenu/ItemCategoryMenu';
 
 class ItemLabel extends PureComponent {
+  state = {
+    anchorEl: null
+  };
+
+  onClick = event => {
+    console.log(event.currentTarget);
+
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  onClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
     const { classes, color } = this.props;
 
+    const { anchorEl } = this.state;
+
+    const open = Boolean(anchorEl);
+
     return (
-      <IconButton
-        aria-label="categorize"
-        classes={{ root: classes.iconButton }}
-        disableRipple
-      >
-        <LabelIcon style={{ color: color }} />
-      </IconButton>
+      <React.Fragment>
+        <IconButton
+          aria-label="categorize"
+          classes={{ root: classes.iconButton }}
+          disableRipple
+          onClick={this.onClick}
+        >
+          <LabelIcon style={{ color: color }} />
+        </IconButton>
+
+        <ItemCategoryMenu
+          anchorEl={anchorEl}
+          onClose={this.onClose}
+          open={open}
+        />
+      </React.Fragment>
     );
   }
 }
