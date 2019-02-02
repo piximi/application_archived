528aca443b1d45ed08d2829d78fb18acf0c569e1
AlexChrisF <Christoph.Friedrich@trackplus.com>
2019-02-02 20:52:33 +0100

    Performance improvements

--- src/components/Gallery/Item.js
+++ src/components/Gallery/Item.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { DragSource } from 'react-dnd';
 import { getEmptyImage } from 'react-dnd-html5-backend';
 import ImageViewerDialog from '../ImageViewer/ImageViewerDialog/ImageViewerDialog';
@@ -43,12 +43,34 @@ function collect(connect, monitor) {
   };
 }
 
-class Item extends PureComponent {
+class Item extends Component {
   constructor(props) {
     super(props);
     this.state = {
       imageViewerDialogOpen: false
     };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.selectedItems.includes(this.props.item.id) !==
+      this.props.selectedItems.includes(this.props.item.id)
+    )
+      return true;
+    else if (JSON.stringify(nextProps.item) !== JSON.stringify(this.props.item))
+      return true;
+    else if (
+      JSON.stringify(nextProps.containerStyle) !==
+      JSON.stringify(this.props.containerStyle)
+    )
+      return true;
+    else if (
+      JSON.stringify(nextProps.containerStyle) !==
+      JSON.stringify(this.props.containerStyle)
+    )
+      return true;
+
+    return false;
   }
 
   closeImageViewerDialog = () => {
