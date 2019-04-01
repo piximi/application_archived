af8c99c5fc3fa22689d1eb07efb93c0de681f97f
Allen Goodman <allen.goodman@icloud.com>
2019-04-01 11:29:30 -0400

    refactored PureComponent classes

--- src/components/SettingsListItem/SettingsListItem.js
+++ src/components/SettingsListItem/SettingsListItem.js
@@ -1,36 +1,22 @@
 import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import React, { PureComponent } from 'react';
-import styles from './SettingsListItem.css';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
 import SettingsIcon from '@material-ui/icons/Settings';
 import SettingsDialog from '../SettingsDialog/SettingsDialog';
 
-class SettingsListItem extends PureComponent {
-  state = {
-    open: false
-  };
+export default function SettingsListItem() {
+  const [open, setOpen] = useState(0);
 
-  toggle = () => {
-    this.setState({
-      open: !this.state.open
-    });
-  };
+  return (
+    <React.Fragment>
+      <ListItem dense button onClick={() => setOpen(!open)}>
+        <ListItemIcon>
+          <SettingsIcon />
+        </ListItemIcon>
 
-  render() {
-    return (
-      <React.Fragment>
-        <ListItem dense button onClick={this.toggle}>
-          <ListItemIcon>
-            <SettingsIcon />
-          </ListItemIcon>
+        <ListItemText primary="Settings" />
+      </ListItem>
 
-          <ListItemText primary="Settings" />
-        </ListItem>
-
-        <SettingsDialog onClose={this.toggle} open={this.state.open} />
-      </React.Fragment>
-    );
-  }
+      <SettingsDialog onClose={() => setOpen(!open)} open={open} />
+    </React.Fragment>
+  );
 }
-
-export default withStyles(styles, { withTheme: true })(SettingsListItem);
