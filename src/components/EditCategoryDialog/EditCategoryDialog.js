18cf7fa4b9bfc699f95e4fcd83efe67acce6840e
Allen Goodman <allen.goodman@icloud.com>
2019-04-02 09:28:35 -0400

    refactored Component classes

--- src/components/EditCategoryDialog/EditCategoryDialog.js
+++ src/components/EditCategoryDialog/EditCategoryDialog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './EditCategoryDialog.css';
 import { withStyles } from '@material-ui/core/styles';
 import {
@@ -14,128 +14,97 @@ import {
 import LabelIcon from '@material-ui/icons/Label';
 import ColorPicker from '../ColorPicker/ColorPicker';
 
-class EditCategoryDialog extends Component {
-  state = {
-    anchor: null,
-    color: '#FF0000',
-    description: ''
-  };
+function EditCategoryDialog(props) {
+  const [anchor, setAnchor] = useState(null);
 
-  componentDidMount() {
-    this.setState({
-      color: this.props.color,
-      description: this.props.description
-    });
+  const [color, setColor] = useState('#FF0000');
+
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    setColor(props.color);
+    setDescription(props.description);
+  });
+
+  function onColorChange(color, event) {
+    setAnchor(null);
+
+    setColor(color.hex);
   }
 
-  onColorChange = (color, event) => {
-    this.setState({
-      ...this.state,
-      anchor: null,
-      color: color.hex
-    });
-  };
+  const {
+    classes,
+    updateCategory,
+    onClose,
+    open,
+    categoryId,
+    categories
+  } = props;
 
-  onDescriptionChange = event => {
-    this.setState({
-      ...this.state,
-      description: event.target.value
-    });
-  };
-
-  openCreateCategoryColorMenu = event => {
-    this.setState({
-      anchor: event.currentTarget
-    });
-  };
-
-  closePopover = () => {
-    this.setState({
-      anchor: null
-    });
-  };
-
-  render() {
-    const {
-      classes,
-      updateCategory,
-      onClose,
-      open,
-      categoryId,
-      categories
-    } = this.props;
-    return (
-      <Dialog open={open} onClose={onClose}>
-        <DialogContent>
-          <div className={classes.margin}>
-            <Grid container spacing={8} alignItems="flex-end">
-              <Grid item>
-                <ButtonBase
-                  aria-haspopup="true"
-                  aria-owns={this.state.anchor ? 'create-category-color' : null}
-                  onClick={this.openCreateCategoryColorMenu}
-                  style={{
-                    color: this.state.color
-                  }}
-                >
-                  <LabelIcon />
-                </ButtonBase>
-              </Grid>
-
-              <Grid item>
-                <TextField
-                  id="create-category-description"
-                  label="Description"
-                  onChange={this.onDescriptionChange}
-                  value={this.state.description}
-                />
-              </Grid>
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogContent>
+        <div className={classes.margin}>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <ButtonBase
+                aria-haspopup="true"
+                aria-owns={anchor ? 'create-category-color' : null}
+                onClick={e => setAnchor(e.currentTarget)}
+                style={{
+                  color: color
+                }}
+              >
+                <LabelIcon />
+              </ButtonBase>
             </Grid>
-          </div>
-        </DialogContent>
 
-        <DialogActions>
-          <Button color="primary" onClick={onClose}>
-            Cancel
-          </Button>
-          <Button
-            color="primary"
-            onClick={() => {
-              onClose();
-              updateCategory(
-                categoryId,
-                this.state.description,
-                this.state.color
-              );
-            }}
-          >
-            Save
-          </Button>
-        </DialogActions>
+            <Grid item>
+              <TextField
+                id="create-category-description"
+                label="Description"
+                onChange={e => setDescription(e.target.value)}
+                value={description}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </DialogContent>
 
-        <Popover
-          open={Boolean(this.state.anchor)}
-          anchorEl={this.state.anchor}
-          onClose={this.closePopover}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center'
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center'
+      <DialogActions>
+        <Button color="primary" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button
+          color="primary"
+          onClick={() => {
+            onClose();
+            updateCategory(categoryId, description, color);
           }}
         >
-          <div className={classes.colorPicker}>
-            <ColorPicker
-              categories={categories}
-              onChange={this.onColorChange}
-            />
-          </div>
-        </Popover>
-      </Dialog>
-    );
-  }
+          Save
+        </Button>
+      </DialogActions>
+
+      <Popover
+        open={Boolean(anchor)}
+        anchorEl={anchor}
+        onClose={() => setAnchor(null)}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+      >
+        <div className={classes.colorPicker}>
+          <ColorPicker categories={categories} onChange={onColorChange} />
+        </div>
+      </Popover>
+    </Dialog>
+  );
 }
 
 export default withStyles(styles, { withTheme: true })(EditCategoryDialog);
