e6e7c6e0a3ec5be20b312f534b3b80c965a908d6
Allen Goodman <allen.goodman@icloud.com>
2019-04-08 14:20:29 -0400

    sidebar refactor

--- src/components/SidebarDrawer/SidebarDrawer/SidebarDrawer.js
+++ src/components/SidebarDrawer/SidebarDrawer/SidebarDrawer.js
@@ -1,15 +1,7 @@
-import {
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText
-} from '@material-ui/core';
+import { Divider, Drawer, List } from '@material-ui/core';
 import React from 'react';
 import styles from './SidebarDrawer.css';
 import ConnectedCategories from '../../../containers/ConnectedCategories';
-import FolderOpenIcon from '@material-ui/icons/FolderOpen';
 import SidebarSaveListItem from '../SidebarSaveListItem/SidebarSaveListItem';
 import SidebarAppBar from '../SidebarAppBar/SidebarAppBar';
 import SidebarModelList from '../SidebarModelList/SidebarModelList';
@@ -18,20 +10,9 @@ import SidebarOpenSampleListItem from '../SidebarOpenSampleListItem/SidebarOpenS
 import HelpListItem from '../SidebarHelpListItem/SidebarHelpListItem';
 import { makeStyles } from '@material-ui/styles';
 import SidebarSendFeedbackListItem from '../SidebarSendFeedbackListItem/SidebarSendFeedbackListItem';
+import SidebarOpenProjectListItem from '../SidebarOpenProjectListItem/SidebarOpenProjectListItem';
 
 const useStyles = makeStyles(styles);
-
-function readDataFromCytoFile(e, props) {
-  const reader = new FileReader();
-
-  reader.onload = function(e) {
-    const text = reader.result;
-    const data = JSON.parse(text);
-    props.updateStore(data);
-  };
-
-  reader.readAsText(e.target.files[0]);
-}
 
 export default function SidebarDrawer(props) {
   const classes = useStyles();
@@ -57,22 +38,7 @@ export default function SidebarDrawer(props) {
       <SidebarAppBar toggle={toggle} toggled={toggled} />
 
       <List dense>
-        <input
-          style={{ display: 'none' }}
-          type="file"
-          accept=".cyto"
-          name="file"
-          id="open-project"
-          onChange={e => readDataFromCytoFile(e, props)}
-        />
-        <label htmlFor="open-project">
-          <ListItem dense button>
-            <ListItemIcon>
-              <FolderOpenIcon />
-            </ListItemIcon>
-            <ListItemText inset primary="Open .cyto" />
-          </ListItem>
-        </label>
+        <SidebarOpenProjectListItem />
 
         <SidebarOpenSampleListItem loadDemoProject={loadDemoProject} />
 
