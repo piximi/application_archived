b707705b2ddbbf70a530d0d19d557690404ea847
Allen Goodman <allen.goodman@icloud.com>
2019-04-08 13:12:53 -0400

    storybook cleanup

--- src/stories/SidebarDrawer/SidebarCreateCategoryListItem.js
+++ src/stories/SidebarDrawer/SidebarCreateCategoryListItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import CreateCategoryListItem from '../components/SidebarDrawer/SidebarCreateCategoryListItem/SidebarCreateCategoryListItem';
+import CreateCategoryListItem from '../../components/SidebarDrawer/SidebarCreateCategoryListItem/SidebarCreateCategoryListItem';
 
 storiesOf('CreateCategoryListItem', module).add('default', () => (
   <CreateCategoryListItem />
