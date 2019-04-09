a8c011e4a530a752aa3fc22a4c8335c758c77029
Allen Goodman <allen.goodman@icloud.com>
2019-04-09 16:20:25 -0400

    Image refactor

--- src/components/Gallery/Image/Image.js
+++ src/components/Gallery/Image/Image.js
@@ -1,77 +1,64 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Image extends PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      imageStatus: 'loading',
-      image: null,
-      imgHeight: null,
-      imgWidth: null
-    };
-    this.canvas = React.createRef();
-  }
+const Image = props => {
+  const { src, openImageViewerDialog } = props;
 
-  componentDidMount() {
-    this.draw();
-  }
+  const [imageStatus, setImageStatus] = useState('loading');
+  const [image, setImage] = useState(null);
+  const [imgHeight, setImgHeight] = useState(null);
+  const [imgWidth, setImgWidth] = useState(null);
 
-  componentDidUpdate() {
-    this.draw();
-  }
+  let canvasRef = useRef();
 
-  // Set image data as state
-  handleLoadedImage = e => {
+  const onLoad = e => {
     const image = e.target;
     const width = image.width;
     const height = image.height;
-    this.setState({
-      imageStatus: 'loaded',
-      image: image,
-      imgHeight: height,
-      imgWidth: width
-    });
+    setImageStatus('loaded');
+    setImage(image);
+    setImgHeight(height);
+    setImgWidth(width);
     image.style.height = '0px';
   };
 
-  handleImageErrored() {
-    this.setState({ imageStatus: 'failed to load' });
-  }
+  const onError = () => {
+    setImageStatus('failed to load');
+  };
 
   // Draw canvas
-  draw = () => {
-    if (this.state.imageStatus === 'loaded') {
-      const canvas = this.canvas.current;
+  const draw = () => {
+    if (imageStatus === 'loaded') {
+      const canvas = canvasRef.current;
       const context = canvas.getContext('2d');
-      canvas.height = this.props.height * 0.9;
-      canvas.width = this.props.width * 0.9;
+      canvas.height = props.height * 0.9;
+      canvas.width = props.width * 0.9;
       const ratio = Math.min(
-        canvas.width / this.state.imgWidth,
-        canvas.height / this.state.imgHeight
+        canvas.width / imgWidth,
+        canvas.height / imgHeight
       );
-      canvas.height = this.state.imgHeight * ratio;
-      canvas.width = this.state.imgWidth * ratio;
+      canvas.height = imgHeight * ratio;
+      canvas.width = imgWidth * ratio;
 
       // Apply filters to context
       context.filter =
         'brightness(' +
-        this.props.brightness +
+        props.brightness +
         '%)  contrast(' +
-        this.props.contrast +
+        props.contrast +
         '%)';
 
-      context.drawImage(this.state.image, 0, 0, canvas.width, canvas.height);
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
       // Apply selected channel filter
       const pixel = context.getImageData(0, 0, canvas.width, canvas.height);
       let data = pixel.data;
-      this.selectVisibleChannels(data, this.props.unselectedChannels);
+      selectVisibleChannels(data, props.unselectedChannels);
       context.putImageData(pixel, 0, 0);
     }
   };
 
-  selectVisibleChannels = (imageData, nonVisibleChannels) => {
+  const selectVisibleChannels = (imageData, nonVisibleChannels) => {
     for (let i = 0; i < imageData.length; i += 4) {
       for (let j = 0; j < 4; j += 1) {
         if (nonVisibleChannels.includes(j)) imageData[j + i] = 0;
@@ -79,29 +66,30 @@ class Image extends PureComponent {
     }
   };
 
-  render() {
-    const { src, openImageViewerDialog } = this.props;
-    return (
-      <React.Fragment>
-        <canvas
-          onDoubleClick={openImageViewerDialog}
-          type={'selectableElement'}
-          style={{ verticalAlign: 'middle', padding: '2px' }}
-          ref={this.canvas}
-          height={this.props.height}
-          width={this.props.width}
-        />
-        <img
-          onLoad={this.handleLoadedImage}
-          onError={this.handleImageErrored.bind(this)}
-          alt="foo"
-          src={src}
-          style={{ visibility: 'hidden' }}
-        />
-      </React.Fragment>
-    );
-  }
-}
+  useEffect(() => {
+    draw();
+  });
+
+  return (
+    <React.Fragment>
+      <canvas
+        onDoubleClick={openImageViewerDialog}
+        type={'selectableElement'}
+        style={{ verticalAlign: 'middle', padding: '2px' }}
+        ref={canvasRef}
+        height={props.height}
+        width={props.width}
+      />
+      <img
+        onLoad={onLoad}
+        onError={onError}
+        alt="foo"
+        src={src}
+        style={{ visibility: 'hidden' }}
+      />
+    </React.Fragment>
+  );
+};
 
 Image.propTypes = {
   src: PropTypes.string,
