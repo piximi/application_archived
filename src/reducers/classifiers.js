c0c48000d1ae6216738c7542d289fc46481cc5d7
Allen Goodman <allen.goodman@icloud.com>
2019-04-10 20:25:38 -0400

    classifier reducer

---
+++ src/reducers/classifiers.js
@@ -0,0 +1,20 @@
+import { createAction, createReducer } from 'redux-starter-kit';
+
+const createClassifier = createAction('classifiers/create');
+
+const updateClassifierName = createAction('classifiers/update/name');
+
+const classifiers = createReducer([], {
+  [createClassifier]: (state, action) => {
+    const classifier = action.payload;
+
+    state.push(classifier);
+  },
+  [updateClassifierName]: (state, action) => {
+    const { index, name } = action.payload;
+
+    const classifier = state[index];
+
+    classifier.name = name;
+  }
+});
