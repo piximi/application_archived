adc19b8380295cc1a76b99875b813ce5c170d3e5
Allen Goodman <allen.goodman@icloud.com>
2019-04-10 21:23:06 -0400

    remove unused import

--- src/hooks/Menu.js
+++ src/hooks/Menu.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function useMenu() {
   const [anchorEl, setAnchorEl] = useState();
