19f18621639531a15a0af8728147b703a7ebef89
Allen Goodman <allen.goodman@icloud.com>
2019-04-17 18:12:49 -0400

    de and fi translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,7 +1,13 @@
+import de from './de';
 import en from './en';
+import fi from './fi';
+import hu from './hu';
 
 const resources = {
-  en: en
+  de: de,
+  en: en,
+  fi: fi,
+  hu: hu
 };
 
 export default resources;
