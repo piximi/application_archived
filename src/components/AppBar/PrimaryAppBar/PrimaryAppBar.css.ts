bed8e54e587e49097faa3162b75369bce3432e81
Allen Goodman <allen.goodman@icloud.com>
2019-04-17 16:58:13 -0400

    fix padding issues

--- src/components/AppBar/PrimaryAppBar/PrimaryAppBar.css.ts
+++ src/components/AppBar/PrimaryAppBar/PrimaryAppBar.css.ts
@@ -1,49 +1,53 @@
-import {createStyles} from "@material-ui/styles";
-import {Theme} from "@material-ui/core";
+import { createStyles } from '@material-ui/styles';
+import { Theme } from '@material-ui/core';
 
 const drawerWidth = 280;
 
-const styles = (theme: Theme) => createStyles({
-  appBar: {
-    backgroundColor: 'rgba(0, 0, 0, 0)',
-    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-    boxShadow: 'none',
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  appBarShiftLeft: {
-    marginLeft: drawerWidth
-  },
-  toolBar: {
-    justifyContent: 'space-between'
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20
-  },
-  hide: {
-    display: 'none'
-  },
-  search: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit
-  },
-  slider: {
-    root: {
-      width: 10
+const styles = (theme: Theme) =>
+  createStyles({
+    appBar: {
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+      boxShadow: 'none',
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    appBarShiftLeft: {
+      marginLeft: drawerWidth
+    },
+    toolBar: {
+      justifyContent: 'space-between'
+    },
+    menuButton: {
+      marginLeft: 12,
+      marginRight: 20
+    },
+    hide: {
+      display: 'none'
+    },
+    search: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit
+    },
+    padding: {
+      marginRight: theme.spacing.unit
     },
     slider: {
-      padding: '22px 0px'
+      root: {
+        width: 10
+      },
+      slider: {
+        padding: '22px 0px'
+      }
     }
-  }
-});
+  });
 
 export default styles;
--- src/components/AppBar/Search/Search.css.ts
+++ src/components/AppBar/Search/Search.css.ts
@@ -1,15 +1,18 @@
-import {Theme} from "@material-ui/core";
-import {createStyles} from "@material-ui/styles";
+import { Theme } from '@material-ui/core';
+import { createStyles } from '@material-ui/styles';
 
-const styles = (theme: Theme) => createStyles({
-  formControl: {
-    width: '100%'
-  },
-  input: {
-    backgroundColor: '#f1f3f4',
-    borderRadius: theme.spacing.unit,
-    padding: theme.spacing.unit
-  }
-});
+const styles = (theme: Theme) =>
+  createStyles({
+    form: {
+      root: {
+        marginRight: theme.spacing.unit
+      }
+    },
+    input: {
+      backgroundColor: '#f1f3f4',
+      borderRadius: theme.spacing.unit,
+      padding: theme.spacing.unit
+    }
+  });
 
 export default styles;
