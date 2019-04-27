f832b41ed8394229dbf4eaac793a7f335f6e7c9d
Allen Goodman <allen.goodman@icloud.com>
2019-04-27 17:43:10 +0900

    typescript refactor

--- src/pages/image/index.ts
+++ src/pages/image/index.ts
@@ -2,7 +2,7 @@ import { BrightnessSlider } from './BrightnessSlider/BrightnessSlider';
 import { ChannelSelection } from './ChannelSelection/ChannelSelection';
 import { Contrast } from './Contrast/Contrast';
 import { ImageHistogram } from './ImageHistogram/ImageHistogram';
-import ImageViewer from './ImageViewer/ImageViewer';
+import { ImageViewer } from './ImageViewer/ImageViewer';
 import { ImageViewerDialog } from './ImageViewerDialog/ImageViewerDialog';
 import { ImageViewerExposureDrawer } from './ImageViewerExposureDrawer/ImageViewerExposureDrawer';
 
