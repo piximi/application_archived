cead8bd680daf38161595bafdc0d0171e4d3409a
Allen Goodman <allen.goodman@icloud.com>
2019-05-06 19:01:24 -0400

    remove dead code

--- src/actions/images.ts
+++ src/actions/images.ts
@@ -16,12 +16,6 @@ import {
   DELETE_IMAGES
 } from '../constants';
 
-// Call to add images to the store
-export const addImagesAction = (images: {}) => ({
-  type: ADD_IMAGES,
-  images
-});
-
 // Call to update the category of an image
 export const updateImageCategoryAction = (
   imgIdentifiers: any,
@@ -34,35 +28,16 @@ export const updateImageCategoryAction = (
   categoryName
 });
 
-// Call to update the visibility of all images with a certain category
-export const updateImageVisibilityBasedOnCategoryAction = (
-  categoryIdentifier: any,
-  value: any
-) => ({
-  type: UPDATE_IMAGE_VISIBILTY_BASED_ON_CATEGORY,
-  categoryIdentifier,
-  value
-});
-
 export const updateImageVisibilityAction = (images: any) => ({
   type: UPDATE_IMAGE_VISIBILITY,
   images
 });
 
-// Call to only show images of a certain category
-export const onlyShowImagesWithCertainCategoryAction = (categoryIdentifier: any) => ({
-  type: ONLY_SHOW_IMAGES_WITH_CERTAIN_CATEGORY,
-  categoryIdentifier
-});
-
-// Call to update images with a certain category, set category to null
-export const setImageCategoryToNullBasedOnCategoryAction = (categoryIdentifier: any) => ({
-  type: SET_IMAGE_CATEGORY_TO_NULL_BASED_ON_CATEGORY,
-  categoryIdentifier
-});
-
 // Call to update the image probalbilty
-export const updateImageProbabilityAction = (imgIdentifiers: any, probability: any) => ({
+export const updateImageProbabilityAction = (
+  imgIdentifiers: any,
+  probability: any
+) => ({
   type: UPDATE_PROBABILITY,
   imgIdentifiers,
   probability
@@ -75,7 +50,10 @@ export const updateCategoryAndProbabilityAction = (predictions: {}) => ({
 });
 
 // Call to update image brightness
-export const updateBrightnessAction = (imgIdentifier: any, brightness: any) => ({
+export const updateBrightnessAction = (
+  imgIdentifier: any,
+  brightness: any
+) => ({
   type: UPDATE_BRIGHTNESS,
   imgIdentifier,
   brightness
@@ -111,7 +89,9 @@ export const updateUnselectedChannelsAction = (
 });
 
 // Call to update image unselected channels  for all images
-export const updateUnselectedChannelsForAllImagesAction = (unselectedChannels: any) => ({
+export const updateUnselectedChannelsForAllImagesAction = (
+  unselectedChannels: any
+) => ({
   type: UPDATE_UNSELECTED_CHANNELS_FOR_ALL_IMAGES,
   unselectedChannels
 });
