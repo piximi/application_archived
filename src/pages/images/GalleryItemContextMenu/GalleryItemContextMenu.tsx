28bd6da5ecada398f5d4a6bd72f03c1392fd7d68
Csaba Molnar <molnar.csaba@brc.mta.hu>
2019-05-09 17:43:37 +0200

    cleanup

--- src/pages/images/GalleryItemContextMenu/GalleryItemContextMenu.tsx
+++ src/pages/images/GalleryItemContextMenu/GalleryItemContextMenu.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import {
-  IconButton,
   ListItemIcon,
   ListItemText,
   MenuItem,
   MenuList,
   Paper,
-  Popover,
-  Backdrop
+  Popover
 } from '@material-ui/core';
 import ColorLensIcon from '@material-ui/icons/ColorLens';
-import DeleteIcon from '@material-ui/icons/Delete';
 import { useTranslation } from 'react-i18next';
 
 const GalleryItemContextMenu = (props: any) => {
@@ -25,8 +22,6 @@ const GalleryItemContextMenu = (props: any) => {
 
   return (
     <Popover
-      // BackdropComponent={<Backdrop open={false}></Backdrop>}
-      // BackdropProps={{ open: false, style:{pointerEvents: 'none'} }}
       anchorPosition={anchorPosition}
       anchorReference="anchorPosition"
       onClose={onClose}
@@ -44,17 +39,6 @@ const GalleryItemContextMenu = (props: any) => {
               <ColorLensIcon />
             </ListItemIcon>
             <ListItemText inset primary={translation('Open image viewer')} />
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              onClose();
-              // delete image
-            }}
-          >
-            <ListItemIcon>
-              <DeleteIcon />
-            </ListItemIcon>
-            <ListItemText inset primary={translation('Delete images')} />
           </MenuItem>
         </MenuList>
       </Paper>
