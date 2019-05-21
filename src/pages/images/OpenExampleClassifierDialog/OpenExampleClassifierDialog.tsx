0d2567502896c8a44702c6ccd8aeba806823db9f
Allen Goodman <allen.goodman@icloud.com>
2019-05-21 15:34:00 -0400

    OpenExampleClassifierListItem

--- src/pages/images/OpenExampleClassifierDialog/OpenExampleClassifierDialog.tsx
+++ src/pages/images/OpenExampleClassifierDialog/OpenExampleClassifierDialog.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
 import styles from './OpenExampleClassifierDialog.css';
 import {
-  Avatar,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
   IconButton,
   List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
   Typography
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { makeStyles } from '@material-ui/styles';
 import { useTranslation } from 'react-i18next';
-import AddIcon from '@material-ui/icons/Add';
 
 // @ts-ignore
 import CIFAR10 from './CIFAR-10.png';
@@ -26,6 +21,7 @@ import CIFAR100 from './CIFAR-100.png';
 
 // @ts-ignore
 import MNIST from './MNIST.png';
+import OpenExampleClassifierListItem from '../OpenExampleClassifierListItem/OpenExampleClassifierListItem';
 
 const useStyles = makeStyles(styles);
 
@@ -54,59 +50,35 @@ const OpenExampleClassifierDialog = (props: any) => {
 
       <DialogContent classes={{ root: classes.dialogContent }}>
         <List>
-          <ListItem
-            button
+          <OpenExampleClassifierListItem
+            src={CIFAR10}
+            primary="CIFAR-10"
+            secondary="The CIFAR-10 dataset contains 10,000 32×32 color photographs in 10 different categories."
             onClick={() => {
               onClose();
               loadDemoProject('cifar10');
             }}
-          >
-            <ListItemAvatar>
-              <Avatar src={CIFAR10}>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="CIFAR-10"
-              secondary="The CIFAR-10 dataset contains 10,000 32×32 color photographs in 10 different categories."
-            />
-          </ListItem>
+          />
 
-          <ListItem
-            button
+          <OpenExampleClassifierListItem
+            src={CIFAR100}
+            primary="CIFAR-100"
+            secondary="The CIFAR-100 dataset consists of 10,000 32 × 32 color photographs in 100 classes."
             onClick={() => {
               onClose();
-              loadDemoProject('cifar10');
+              loadDemoProject('cifar100');
             }}
-          >
-            <ListItemAvatar>
-              <Avatar src={CIFAR100}>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="CIFAR-100"
-              secondary="The CIFAR-100 dataset consists of 10,000 32 × 32 color photographs in 100 classes."
-            />
-          </ListItem>
+          />
 
-          <ListItem
-            button
+          <OpenExampleClassifierListItem
+            src={MNIST}
+            primary="MNIST"
+            secondary="The MNIST dataset consists of 10,000 28 × 28 handwritten digits in 10 classes."
             onClick={() => {
               onClose();
               loadDemoProject('mnist');
             }}
-          >
-            <ListItemAvatar>
-              <Avatar src={MNIST}>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="MNIST"
-              secondary="The MNIST dataset consists of 10,000 28 × 28 handwritten digits in 10 classes."
-            />
-          </ListItem>
+          />
         </List>
       </DialogContent>
       <DialogActions />
