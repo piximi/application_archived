9f4ebc7b0c17b25fbe9980c25c0b30fee46ad494
Allen Goodman <allen.goodman@icloud.com>
2019-06-12 11:08:49 -0400

    updated snapshots

--- src/pages/images/Logo/Logo.test.tsx
+++ src/pages/images/Logo/Logo.test.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
 import Logo from './Logo';
 
-describe('Logo', () => {
-  it('should render', () => {
-    expect(1 + 1).toBe(2);
-  });
+it('renders without crashing', () => {
+  shallow(<Logo />);
 });
