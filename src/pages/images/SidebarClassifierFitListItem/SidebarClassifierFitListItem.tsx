1e0a297e8d78329b89126b339420c6e59bdfa164
Allen Goodman <allen.goodman@icloud.com>
2019-06-13 13:12:12 -0400

    form

--- src/pages/images/SidebarClassifierFitListItem/SidebarClassifierFitListItem.tsx
+++ src/pages/images/SidebarClassifierFitListItem/SidebarClassifierFitListItem.tsx
@@ -10,9 +10,13 @@ import {
   ExpansionPanel,
   ExpansionPanelSummary,
   ExpansionPanelDetails,
-  Slide
+  Slide,
+  TextField,
+  MenuItem,
+  Grid
 } from '@material-ui/core';
 import * as React from 'react';
+import MaskedInput from 'react-text-mask';
 import { useState } from 'react';
 import ScatterPlotIcon from '@material-ui/icons/ScatterPlot';
 import { useTranslation } from 'react-i18next';
@@ -21,17 +25,84 @@ import { Snackbar } from '@piximi/components';
 import { createDataset, createModel } from '../../../network';
 import { Logs } from '@tensorflow/tfjs-layers';
 import * as tensorflow from '@tensorflow/tfjs';
-import { Close, ArrowBack } from '@material-ui/icons';
+import { ArrowBack, ExpandMore } from '@material-ui/icons';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { TransitionProps } from '@material-ui/core/transitions';
 
+const lossFunctions = [
+  {
+    label: 'Absolute difference',
+    value: 'absoluteDifference'
+  },
+  {
+    label: 'Cosine distance',
+    value: 'cosineDistance'
+  },
+  {
+    label: 'Hinge',
+    value: 'hingeLoss'
+  },
+  {
+    label: 'Huber',
+    value: 'huberLoss'
+  },
+  {
+    label: 'Log',
+    value: 'logLoss'
+  },
+  {
+    label: 'Mean squared error (MSE)',
+    value: 'meanSquaredError'
+  },
+  {
+    label: 'Sigmoid cross entropy',
+    value: 'sigmoidCrossEntropy'
+  },
+  {
+    label: 'Softmax cross entropy',
+    value: 'softmaxCrossEntropy'
+  }
+];
+
+const optimizationAlgorithms = [
+  {
+    value: 'adadelta',
+    label: 'Adadelta'
+  },
+  {
+    value: 'adam',
+    label: 'Adam'
+  },
+  {
+    value: 'adamax',
+    label: 'Adamax'
+  },
+  {
+    value: 'rmsprop',
+    label: 'RMSProp'
+  },
+  {
+    value: 'sgd',
+    label: 'Stochastic gradient descent (SGD)'
+  }
+];
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    expansionPanel: {
+      boxShadow: 'none'
+    },
+    form: {},
     appBar: {
       backgroundColor: 'rgb(255, 255, 255)',
       borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
       boxShadow: 'none',
       position: 'relative'
+    },
+    container: {
+      width: '100%'
+      // display: 'flex',
+      // flexWrap: 'wrap',
     },
     root: {
       zIndex: 1100
@@ -41,6 +112,14 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     paperFullScreen: {
       left: '280px'
+    },
+    menu: {
+      // width: 200,
+    },
+    textField: {
+      // marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: '100%'
     },
     title: {
       marginLeft: theme.spacing(2),
@@ -53,6 +132,41 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+interface TextMaskCustomProps {
+  inputRef: (ref: HTMLInputElement | null) => void;
+}
+
+function TextMaskCustom(props: TextMaskCustomProps) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={(ref: any) => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={[
+        '(',
+        /[1-9]/,
+        /\d/,
+        /\d/,
+        ')',
+        ' ',
+        /\d/,
+        /\d/,
+        /\d/,
+        '-',
+        /\d/,
+        /\d/,
+        /\d/,
+        /\d/
+      ]}
+      placeholderChar={'\u2000'}
+      showMask
+    />
+  );
+}
+
 const FitclassifierDialogTransition = React.forwardRef<
   unknown,
   TransitionProps
@@ -62,6 +176,22 @@ const FitclassifierDialogTransition = React.forwardRef<
 
 const FitclassifierDialog = (props: any) => {
   const { openedDialog, closeDialog } = props;
+
+  interface State {
+    lossFunction: string;
+    optimizationAlgorithm: string;
+  }
+
+  const [values, setValues] = React.useState<State>({
+    lossFunction: 'softmaxCrossEntropy',
+    optimizationAlgorithm: 'adam'
+  });
+
+  const onChange = (name: keyof State) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
   const classes = useStyles();
 
@@ -91,22 +221,119 @@ const FitclassifierDialog = (props: any) => {
         </Toolbar>
       </AppBar>
 
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ArrowBack />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>Expansion Panel 1</Typography>
-        </ExpansionPanelSummary>
+      <Grid container spacing={0}>
+        <form className={classes.container} noValidate autoComplete="off">
+          <Grid item xs={4}>
+            <ExpansionPanel classes={{ root: classes.expansionPanel }}>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="optimization-content"
+                id="optimization-header"
+              >
+                <Typography className={classes.heading}>
+                  Optimization
+                </Typography>
+              </ExpansionPanelSummary>
 
-        <ExpansionPanelDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+              <ExpansionPanelDetails>
+                <Grid container spacing={2}>
+                  <Grid item xs>
+                    <TextField
+                      id="optimization-algorithm"
+                      select
+                      label="Optimization algorithm"
+                      className={classes.textField}
+                      value={values.optimizationAlgorithm}
+                      onChange={onChange('optimizationAlgorithm')}
+                      SelectProps={{
+                        MenuProps: {
+                          className: classes.menu
+                        }
+                      }}
+                      margin="normal"
+                    >
+                      {optimizationAlgorithms.map(optimizationAlgorithm => (
+                        <MenuItem
+                          key={optimizationAlgorithm.value}
+                          value={optimizationAlgorithm.value}
+                        >
+                          {optimizationAlgorithm.label}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                  </Grid>
+
+                  <Grid item xs>
+                    <TextField
+                      id="learning-rate"
+                      label="Learning rate"
+                      className={classes.textField}
+                      value={'0.01'}
+                      onChange={() => {}}
+                      margin="normal"
+                    />
+                  </Grid>
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grid>
+
+          <Grid item xs={4}>
+            <ExpansionPanel classes={{ root: classes.expansionPanel }}>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="optimization-content"
+                id="optimization-header"
+              >
+                <Typography className={classes.heading}>
+                  Optimization
+                </Typography>
+              </ExpansionPanelSummary>
+
+              <ExpansionPanelDetails>
+                <Grid container spacing={2}>
+                  <Grid item xs>
+                    <TextField
+                      id="learning-rate"
+                      label="Learning rate"
+                      className={classes.textField}
+                      value={''}
+                      onChange={() => {}}
+                      margin="normal"
+                    />
+                  </Grid>
+
+                  <Grid item xs>
+                    <TextField
+                      id="loss-function"
+                      select
+                      label="Loss function"
+                      className={classes.textField}
+                      value={values.lossFunction}
+                      onChange={onChange('lossFunction')}
+                      SelectProps={{
+                        MenuProps: {
+                          className: classes.menu
+                        }
+                      }}
+                      margin="normal"
+                    >
+                      {lossFunctions.map(lossFunction => (
+                        <MenuItem
+                          key={lossFunction.value}
+                          value={lossFunction.value}
+                        >
+                          {lossFunction.label}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                  </Grid>
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grid>
+        </form>
+      </Grid>
     </Dialog>
   );
 };
