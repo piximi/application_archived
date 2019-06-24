155532f7c26427d03ce1f24866e7aee1ea7a43eb
Allen Goodman <allen.goodman@icloud.com>
2019-06-23 20:16:17 -0400

    updated create-react-scripts boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as tf from '@tensorflow/tfjs';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import './i18n';
 import { store, persistor } from '@piximi/store';
-
-initializeModel();
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,13 +17,6 @@ ReactDOM.render(
   document.getElementById('root') as HTMLElement
 );
 
-registerServiceWorker();
-
-async function initializeModel() {
-  const preloadedModel = await tf.loadLayersModel(
-    'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json'
-  );
-  await preloadedModel.save('indexeddb://classifier');
-}
+serviceWorker.register();
 
 export { store };
