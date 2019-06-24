e4e405985a9b3615606e865fcfe6e42b4da09dbc
Allen Goodman <allen.goodman@icloud.com>
2019-06-23 20:01:00 -0400

    fixed React.forwardRef issue

--- src/pages/images/SidebarSettingsListItem/SidebarSettingsListItem.tsx
+++ src/pages/images/SidebarSettingsListItem/SidebarSettingsListItem.tsx
@@ -1,24 +1,16 @@
 import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import * as React from 'react';
 import SettingsIcon from '@material-ui/icons/Settings';
-import { SettingsDialog } from '../../settings';
-import { useDialog } from '@piximi/hooks';
 
 const SettingsListItem = () => {
-  const { openedDialog, openDialog, closeDialog } = useDialog();
-
   return (
-    <React.Fragment>
-      <ListItem dense button disabled onClick={openDialog}>
-        <ListItemIcon>
-          <SettingsIcon />
-        </ListItemIcon>
+    <ListItem dense button disabled>
+      <ListItemIcon>
+        <SettingsIcon />
+      </ListItemIcon>
 
-        <ListItemText primary="Settings" />
-      </ListItem>
-
-      <SettingsDialog onClose={closeDialog} open={openedDialog} />
-    </React.Fragment>
+      <ListItemText primary="Settings" />
+    </ListItem>
   );
 };
 
