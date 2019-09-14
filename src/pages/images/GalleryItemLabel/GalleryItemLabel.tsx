71132d002c10ffe2632112fa39a7b5043112f181
moserle <moserle@student.ethz.ch>
2019-09-14 12:01:27 +0200

    show predicted images, resolve #171

--- src/pages/images/GalleryItemLabel/GalleryItemLabel.tsx
+++ src/pages/images/GalleryItemLabel/GalleryItemLabel.tsx
@@ -1,23 +1,49 @@
 import * as React from 'react';
 import LabelIcon from '@material-ui/icons/Label';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
 import styles from './GalleryItemLabel.css';
 import { IconButton } from '@material-ui/core';
 import { ConnectedItemCategoryMenu } from '../../../containers';
 import { makeStyles } from '@material-ui/styles';
 import { useMenu } from '@piximi/hooks';
+import { Image, Category, Score } from '@piximi/types';
 
 const useStyles = makeStyles(styles);
 
-export const GalleryItemLabel = (props: any) => {
+const getLableIndex = (scores: Score[]) => {
+  var maxScore = 0;
+  var lableIndex = 0;
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i].probability > maxScore) {
+      maxScore = scores[i].probability;
+      lableIndex = i;
+    }
+  }
+  return scores[lableIndex].categoryIdentifier;
+};
+
+type GalleryItemLabelProps = {
+  categories: Category[];
+  image: Image;
+};
+
+export const GalleryItemLabel = (props: GalleryItemLabelProps) => {
   const { anchorEl, openedMenu, openMenu, closeMenu } = useMenu();
 
   const { categories, image } = props;
 
   const classes = useStyles({});
 
-  const findCategoryColor = () => {
+  const predictedImage: boolean =
+    image.categoryIdentifier === '00000000-0000-0000-0000-000000000000' &&
+    image.scores.length !== 0;
+  const predictedCategoryIdentifier: string = predictedImage
+    ? getLableIndex(image.scores)
+    : '00000000-0000-0000-0000-000000000000';
+
+  const findCategoryColor = (categoryIdentifier: string) => {
     const index = categories.findIndex((category: any) => {
-      return category.identifier === image.categoryIdentifier;
+      return category.identifier === categoryIdentifier;
     });
 
     if (index > -1) {
@@ -35,7 +61,15 @@ export const GalleryItemLabel = (props: any) => {
         disableRipple
         onClick={openMenu}
       >
-        <LabelIcon style={{ color: findCategoryColor() }} />
+        {!predictedImage ? (
+          <LabelIcon
+            style={{ color: findCategoryColor(image.categoryIdentifier) }}
+          />
+        ) : (
+          <LabelImportantIcon
+            style={{ color: findCategoryColor(predictedCategoryIdentifier) }}
+          />
+        )}
       </IconButton>
 
       <ConnectedItemCategoryMenu
