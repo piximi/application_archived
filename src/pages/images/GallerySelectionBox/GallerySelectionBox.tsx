2400975fc85c986f9f12dbdf85c3de7be6c26af4
Levin Moser <moserle@student.ethz.ch>
2019-11-05 15:57:19 +0100

    Working version (#176)
    * update package.json
    
    * update navigation-drawer package, remove unused code

--- src/pages/images/GallerySelectionBox/GallerySelectionBox.tsx
+++ src/pages/images/GallerySelectionBox/GallerySelectionBox.tsx
@@ -24,33 +24,17 @@ export const GallerySelectionBox = (props: GallerySelectionBoxProps) => {
     borderColor: '#AAAAAA'
   });
 
+  const styleFromBoxCoordinates = reCalc(selectionBoxCoordinates);
+
+  const newStyle = {
+    ...style,
+    ...styleFromBoxCoordinates,
+    visibility: visibility
+  };
+
   React.useEffect(() => {
-    const styleFromBoxCoordinates = reCalc(selectionBoxCoordinates);
-    const newStyle = {
-      ...style,
-      ...styleFromBoxCoordinates,
-      visibility: visibility
-    };
-
-    //setStyle(newStyle);
-  }, [selectionBoxCoordinates, style, visibility]);
-
-  // React.useEffect(() => {
-  //   const defaultCoordinates = {
-  //     left: '0px',
-  //     top: '0px',
-  //     width: '0px',
-  //     height: '0px'
-  //   };
-
-  //   const newStyle = {
-  //     ...style,
-  //     ...defaultCoordinates,
-  //     visibility: visibility
-  //   };
-
-  //   setStyle(newStyle);
-  // }, [selectionBoxCoordinates, style, visibility]);
+    setStyle(newStyle);
+  }, [selectionBoxCoordinates, visibility]);
 
   return <div style={style} />;
 };
