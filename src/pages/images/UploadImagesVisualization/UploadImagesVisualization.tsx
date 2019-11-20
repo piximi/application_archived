763dc6c02cf9223f02f9c2eeff21446486543517
AlexChrisF <Christoph.Friedrich@trackplus.com>
2019-11-20 15:59:16 -0500

    Improve progress visualization

--- src/pages/images/UploadImagesVisualization/UploadImagesVisualization.tsx
+++ src/pages/images/UploadImagesVisualization/UploadImagesVisualization.tsx
@@ -7,10 +7,6 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      // width: '100%',
-      // '& > * + *': {
-      //   marginTop: theme.spacing(2)
-      // }
       padding: '30px',
       width: '450px'
     },
@@ -20,27 +16,9 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function LinearDeterminate() {
+export default function LinearDeterminate(props: any) {
+  const [completed] = props;
   const classes = useStyles();
-  const [completed, setCompleted] = React.useState(0);
-
-  React.useEffect(() => {
-    function progress() {
-      setCompleted(oldCompleted => {
-        if (oldCompleted === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10;
-        return Math.min(oldCompleted + diff, 100);
-      });
-    }
-
-    const timer = setInterval(progress, 500);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
   return (
     <Paper className={classes.root}>
       <Typography variant="h5" component="h3">
