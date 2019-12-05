8c4c90dca7762c0bc689a8a49e83f31d30993e8c
Allen Goodman <allen.goodman@icloud.com>
2019-12-05 12:42:02 -0500

    use @piximi/upload-image-dialog

--- src/pages/images/ImportImagesButton/ImportImagesButton.tsx
+++ src/pages/images/ImportImagesButton/ImportImagesButton.tsx
@@ -3,25 +3,31 @@ import styles from './ImportImagesButton.css';
 import Button from '@material-ui/core/Button';
 import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
 import { makeStyles } from '@material-ui/styles';
-import { FileInput } from '..';
 import { useTranslation } from 'react-i18next';
+import { ConnectedUploadImageDialog } from '@piximi/upload-image-dialog';
+import { useDialog } from '@piximi/hooks';
 
 const useStyles = makeStyles(styles);
 
 export const ImportImagesButton = (props: any) => {
-  const { createImages } = props;
-
   const classes = useStyles({});
+
+  const { openedDialog, closeDialog, openDialog } = useDialog();
 
   const { t: translation } = useTranslation();
 
   return (
-    <FileInput createImages={createImages}>
-      <Button className={classes.button}>
+    <>
+      <Button className={classes.button} onClick={openDialog}>
         <AddPhotoAlternateIcon className={classes.icon} />
 
         {translation('Import images')}
       </Button>
-    </FileInput>
+
+      <ConnectedUploadImageDialog
+        closeDialog={closeDialog}
+        openedDialog={openedDialog}
+      />
+    </>
   );
 };
--- src/pages/images/index.ts
+++ src/pages/images/index.ts
@@ -1,7 +1,6 @@
 export { Application } from './Application/Application';
 export { DeleteButton } from './DeleteButton/DeleteButton';
 export { DeleteImageDialog } from './DeleteImageDialog/DeleteImageDialog';
-export { FileInput } from './FileInput/FileInput';
 export { ImportImagesButton } from './ImportImagesButton/ImportImagesButton';
 export { Logo } from './Logo/Logo';
 export { ConnectedPrimaryAppBar } from './PrimaryAppBar/ConnectedPrimaryAppBar';
